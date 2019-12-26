2af196e6e934b1e94199a601d2c822914e00f454
parz1 <1062134330@qq.com>
2019-12-26 22:42:48 +0800

    5chapter done.

--- Dp/Homework/Dp.cpp
+++ Dp/Homework/Dp.cpp
@@ -44,17 +44,47 @@ int f(int n)
 }
 
 //3-27 Ackerman
-int akm(int m, int n)
+int akm_dp(int m, int n)
 {
     if (!m)
         return n + 1;
     if (!n)
     {
-        return akm(m - 1, 1);
+        return akm_dp(m - 1, 1);
     }
-    return akm(m - 1, akm(m, n - 1));
+    return akm_dp(m - 1, akm_dp(m, n - 1));
 }
-
+//3-27 Ackerman 非递归
+class node
+{
+public:
+    int m;
+    int n;
+};
+int akm_stack(int m, int n)
+{
+    stack<node> s;
+    node e,e1,d;
+    e.m=m;
+    e.n=n;
+    s.push(e);
+    do{
+        while(e.m) {
+            while(e.n){
+                e.n--;
+                s.push(e);
+            }
+            e.m--;e.n=1;
+        }
+        if(s.size()>1) {
+            e1.n=e.n;
+            s.pop();
+            e.m--;
+            e.n=e1.n+1;
+        }
+    }while(s.size()!=1||e.m!=0);
+    return e.n+1;
+}
 int main()
 {
     test3_10();
